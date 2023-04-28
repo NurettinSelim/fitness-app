fe4e914bf955632048b227fc8dad89bde8bb90f6
NurettinSelim <nurettinselim03@gmail.com>
2023-04-28 22:10:55 +0300

    exercise type seperated

---
+++ lib/models/exercise_type.g.dart
@@ -0,0 +1,66 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'exercise_type.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ExerciseTypeAdapter extends TypeAdapter<ExerciseType> {
+  @override
+  final int typeId = 2;
+
+  @override
+  ExerciseType read(BinaryReader reader) {
+    switch (reader.readByte()) {
+      case 0:
+        return ExerciseType.yoga;
+      case 1:
+        return ExerciseType.run;
+      case 2:
+        return ExerciseType.cycle;
+      case 3:
+        return ExerciseType.swim;
+      case 4:
+        return ExerciseType.pilates;
+      case 5:
+        return ExerciseType.cooldown;
+      default:
+        return ExerciseType.yoga;
+    }
+  }
+
+  @override
+  void write(BinaryWriter writer, ExerciseType obj) {
+    switch (obj) {
+      case ExerciseType.yoga:
+        writer.writeByte(0);
+        break;
+      case ExerciseType.run:
+        writer.writeByte(1);
+        break;
+      case ExerciseType.cycle:
+        writer.writeByte(2);
+        break;
+      case ExerciseType.swim:
+        writer.writeByte(3);
+        break;
+      case ExerciseType.pilates:
+        writer.writeByte(4);
+        break;
+      case ExerciseType.cooldown:
+        writer.writeByte(5);
+        break;
+    }
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ExerciseTypeAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
